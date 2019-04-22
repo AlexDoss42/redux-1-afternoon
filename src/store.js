2c5beb8bc95106cba4c37fb0c3103f72c5b71203
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-04-22 15:31:37 -0600

    Completes 5&6

--- src/store.js
+++ src/store.js
@@ -2,11 +2,18 @@ import { createStore } from 'redux';
 
 let initialState = {
   name: '',
-  category: ''
+  category: '',
+  authorFirst: '',
+  authorLast: '',
+  ingredients: [],
+  instructions: []
 };
 
 export const UPDATE_NAME = 'UPDATE_NAME';
 export const UPDATE_CATEGORY = 'UPDATE_CAREGORY';
+export const AUTHOR_FIRST = 'AUTHOR_FIRST';
+export const AUTHOR_LAST = 'AUTHOR_LAST';
+export const ADD_INGREDIENT = 'ADD_INGREDIENTS';
 
 function reducer(state = initialState, action) {
   
@@ -17,6 +24,13 @@ function reducer(state = initialState, action) {
       return { ...state, name: payload};
     case UPDATE_CATEGORY:
       return { ...state, category: payload};
+    case AUTHOR_FIRST:
+      return { ...state, authorFirst: payload};
+    case AUTHOR_LAST:
+      return { ...state, authorLast: payload};
+    case ADD_INGREDIENT:
+      const newIngredients = [...state.ingredients, payload];
+      return {...state, ingredients: newIngredients};
     default: 
       return state;
   }
